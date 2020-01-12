a28a1f3638efd9f61429c6e0ea53ffe8383b51d7
Gian Luca da Silva Figueiredo <luca.gian16@outlook.com>
2020-01-11 22:18:02 -0300

    Add class to handle powershell and command lines execution.

---
+++ DeadFishStudio.SandboxUtilities.Presenter/PowerShellResponsabilities/IPowerShellUtilities.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+
+namespace DeadFishStudio.SandboxUtilities.PowerShellResponsabilities
+{
+    public interface IPowerShellUtilities
+    {
+        List<string> CallPowerShellCommandLine(string name, params string[] args);
+    }
+}
---
+++ DeadFishStudio.SandboxUtilities.Presenter/PowerShellResponsabilities/PowerShellUtilities.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace DeadFishStudio.SandboxUtilities.PowerShellResponsabilities
+{
+    public class PowerShellUtilities : IPowerShellUtilities
+    {
+        public List<string> CallPowerShellCommandLine(string name, params string[] args)
+        {
+            using var runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+
+            using var powerShell = PowerShell.Create();
+            powerShell.Runspace = runspace;
+
+            foreach (var arg in args)
+            {
+                powerShell.AddScript($"{name} {arg}");
+            }
+
+            powerShell.Invoke();
+
+            return ReturnListOfErrors(powerShell.Streams.Error);
+        }
+
+        private static List<string> ReturnListOfErrors(PSDataCollection<ErrorRecord> dataCollections)
+        {
+            var consoleOutput = new List<string>();
+            foreach (var item in dataCollections)
+            {
+                consoleOutput.Add(item.Exception.Message);
+            }
+
+            return consoleOutput;
+        }
+    }
+}
