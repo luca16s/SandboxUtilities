e3b60bac057a9b7aa0d4e1c812023b6d15de5bcd
Gian Luca da Silva Figueiredo <luca.gian16@outlook.com>
2020-01-12 16:27:19 -0300

    Added a class with user default messages.

---
+++ DeadFishStudio.SandboxUtilities.Presenter/Messages/SystemDefaultMessages.cs
@@ -0,0 +1,18 @@
+﻿namespace DeadFishStudio.SandboxUtilities.Messages
+{
+    public static class SystemDefaultMessages
+    {
+        public const string SandboxUpdate = "Sandbox Update";
+        public const string VerifyingRepository = "Verificando repositório:";
+        public const string LineSeparator = @"-------------------------------------------------------------
+                                              #############################################################
+                                              -------------------------------------------------------------";
+        public const string CleanSandbox = "Limpando Sandbox.";
+        public const string MigratingSandbox = "Atualizando Sandbox.";
+        public const string BranchNameMessage = "Insert branch name: ";
+        public const string DataBaseProcessMessage = "Insert database process name: ";
+        public const  string DirectoryPathMessage = "Insert directory path: ";
+        public const  string RepositoryUrlMessage = "Insert Rrepository url: ";
+        public const  string SearchPatternMessage = "Insert search pattern: ";
+    }
+}
